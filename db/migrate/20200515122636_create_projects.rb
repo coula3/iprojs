c5969fa0c6b280bfb747b6d0a6a1c20cf1c95cc2
Alvin Coulson <aaacoulson@gmail.com>
2020-05-15 08:37:36 -0400

    add associations and update migration files

--- db/migrate/20200515122636_create_projects.rb
+++ db/migrate/20200515122636_create_projects.rb
@@ -1,6 +1,18 @@
 class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
+      t.integer :user_id
+      t.string :title
+      t.string :domain
+      t.string :classification
+      t.string :nature
+      t.string :languages
+      t.text :libraries
+      t.string :phase
+      t.text :notes
+      t.datetime :start_date
+      t.datetime :planned_end_date
+      t.datetime :actual_end_date
 
       t.timestamps null: false
     end
