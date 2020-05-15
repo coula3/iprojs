9561f543dfeddaad1b89de784ea844e3bd341676
Alvin Coulson <aaacoulson@gmail.com>
2020-05-15 09:19:49 -0400

    enable session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "iprojs_top_super_secret"
   end
 
   get "/" do
