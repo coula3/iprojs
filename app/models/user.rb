42456ee2fc6b9ba48398260bebafcf3f70f1a450
Alvin Coulson <aaacoulson@gmail.com>
2020-05-17 09:13:03 -0400

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
     has_many :projects
     has_secure_password
+
+    validates :first_name, :last_name, :dob, :email, presence: true
+    validates :password, length: { in: 3..6 }
 end
