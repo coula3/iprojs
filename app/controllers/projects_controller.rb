d9e537d1c15babd30c727cb4f73beaa975905800
Alvin Coulson <aaacoulson@gmail.com>
2020-05-17 02:47:11 -0400

    add projects post route

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -30,7 +30,25 @@ class ProjectsController < ApplicationController
   end
 
   patch "/projects/:id" do
-    redirect "/projects/:id"
+    new_params = Hash.new
+    old_object = Project.find(params[:id])
+    # binding.pry
+    new_params["title"] = params[:title]
+    new_params["domain"] = params[:domain]
+    new_params["classification"] = params[:classification]
+    new_params["nature"] = params[:nature]
+    new_params["languages"] = params[:languages]
+    new_params["libraries"] = params[:libraries]
+    new_params["phase"] = params[:phase]
+    new_params["start_date"] = params[:start_date]
+    new_params["planned_end_date"] = params[:planned_end_date]
+    new_params["actual_end_date"] = params[:actual_end_date]
+    new_params["notes"] = params[:notes]
+    
+    old_object.update(new_params)
+    # binding.pry
+
+    redirect "/projects/#{old_object.id}"
   end
 
   delete "/projects/:id/delete" do
