8a09ddcf9f1f03983734699fbfe642a43c2cafd4
Alvin Coulson <aaacoulson@gmail.com>
2020-05-18 17:10:27 -0400

    add email validator

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
     has_many :projects
     has_secure_password
 
-    validates :first_name, :last_name, :dob, :email, presence: true
+    validates :first_name, :last_name, :dob, presence: true
+    validates :email, presence: true, email: true
     validates_uniqueness_of :email
 end
