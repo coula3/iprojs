9856aca327159a593de7774d5ce4ed8a965a283b
Alvin Coulson <aaacoulson@gmail.com>
2020-05-17 20:55:40 -0400

    add slugifiable modules

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,9 @@
+require_relative 'concerns/slugifiable.rb'
+
 class Project < ActiveRecord::Base
     belongs_to :user
     validates :title, presence: true
+
+    extend Slugifiable::ClassMethods
+    include Slugifiable::InstanceMethods
 end
