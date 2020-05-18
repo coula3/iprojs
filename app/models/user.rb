01e75de216c1105b34cc5d1697a7663ea03e1771
Alvin Coulson <aaacoulson@gmail.com>
2020-05-18 18:41:16 -0400

    change email validation to address similar flash messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
     has_secure_password
 
     validates :first_name, :last_name, :dob, presence: true
-    validates :email, presence: true, email: true
+    validates :email, email: true
     validates_uniqueness_of :email
 end
