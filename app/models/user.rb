db8f58352d0d01ead5d2880dcc8d04fb6eaa6e6d
Alvin Coulson <aaacoulson@gmail.com>
2020-05-18 08:26:01 -0400

    remove length validation on password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,5 @@ class User < ActiveRecord::Base
     has_secure_password
 
     validates :first_name, :last_name, :dob, :email, presence: true
-    validates :password, length: { in: 3..6 }
     validates_uniqueness_of :email
 end
