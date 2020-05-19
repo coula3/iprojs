24ba86869edf6a0247156d9bbe841b26b190125e
Alvin Coulson <aaacoulson@gmail.com>
2020-05-19 10:25:07 -0400

    add validation on 'dob' to ensure user signups meet the age requirement

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ActiveRecord::Base
     validates :first_name, :last_name, :dob, presence: true
     validates :email, email: true
     validates_uniqueness_of :email
+    validate :dob_must_be_at_least_ten_years_old
+
+    def dob_must_be_at_least_ten_years_old
+        if dob.present? && (Date.today.year - dob.year) < 10
+            errors.add(:dob, "must be least 10 years")
+        end
+    end
 end
