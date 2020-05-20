faeeced2f01f92e2534d09509c8c5937f78002df
Alvin Coulson <aaacoulson@gmail.com>
2020-05-20 10:55:48 -0400

    replace references of 'dob' with 'date_of_birth'

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,14 @@ class User < ActiveRecord::Base
     has_many :projects
     has_secure_password
 
-    validates :first_name, :last_name, :dob, presence: true
+    validates :first_name, :last_name, :date_of_birth, presence: true
     validates :email, email: true
     validates_uniqueness_of :email
     validate :dob_must_be_at_least_ten_years_old
 
     def dob_must_be_at_least_ten_years_old
-        if dob.present? && (Date.today.year - dob.year) < 10
-            errors.add(:dob, "must be least 10 years")
+        if date_of_birth.present? && (Date.today.year - date_of_birth.year) < 10
+            errors.add(:date_of_birth, "must be least 10 years")
         end
     end
 end
