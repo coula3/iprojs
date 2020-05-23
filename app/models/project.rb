f7d01e6d1893f6778c25c65ba4a669d0eddc02d2
Alvin Coulson <aaacoulson@gmail.com>
2020-05-22 23:50:46 -0400

    add validations on url

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,8 @@ require_relative 'concerns/slugifiable.rb'
 
 class Project < ActiveRecord::Base
     belongs_to :user
-    validates :title, presence: true
+    validates :title, :url, presence: true
+    validates :url, format: { with: /\:\/+\w+\.[a-z]{2,}/, message: "must be valid" }
 
     extend Slugifiable::ClassMethods
     include Slugifiable::InstanceMethods
