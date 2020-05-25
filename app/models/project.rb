c1c006e04ec5442065063ef634222f42aae67d06
Alvin Coulson <aaacoulson@gmail.com>
2020-05-25 14:43:57 -0400

    fix validation bug on add new product

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,8 +2,9 @@ require_relative 'concerns/slugifiable.rb'
 
 class Project < ActiveRecord::Base
     belongs_to :user
-    validates :title, :url, presence: true
-    validates :url, format: { with: /\:\/+\w+\.[a-z]{2,}/, message: "must be valid" }
+    validates :title, presence: true
+    validates :url, format: { with: /\:\/+\w+\.[a-zA-Z]{2,}/, message: "must be valid" },
+        if: Proc.new { |entry| entry.url.present? }
 
     extend Slugifiable::ClassMethods
     include Slugifiable::InstanceMethods
