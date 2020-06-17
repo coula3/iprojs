c84ea0cf200230d5597fc27ff732b14463b160bb
Alvin Coulson <aaacoulson@gmail.com>
2020-06-17 11:44:55 -0400

    fix bug on user age validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
     has_secure_password
 
     def dob_must_be_at_least_ten_years_old
-        if date_of_birth.present? && (Date.today.year - date_of_birth.year) < 10
+            if date_of_birth.present? && (Date.today.year - date_of_birth.year) < 10
             errors.add(:date_of_birth, "must be least 10 years")
         end
     end
