05bd20bb0a47767c41df4768212292ae767e6023
Alvin Coulson <aaacoulson@gmail.com>
2020-07-09 15:50:59 -0400

    fix bug when self is nil on titlecase model

--- app/models/string.rb
+++ app/models/string.rb
@@ -12,16 +12,18 @@ class String
                 str.capitalize
             end
         end
-
-        first_str = capitalize_array.shift
-        if first_str.include?("-") && capitalize_array.count == 0
-            capitalize_array.unshift(first_str)
-        elsif first_str.include?("-") && capitalize_array.count > 0
-            capitalize_array.unshift(first_str)
-        elsif first_str == first_str.downcase
-            capitalize_array.unshift(first_str.capitalize)
-        else
-            capitalize_array.unshift(first_str.capitalize)
-        end.join(" ")
+        
+        if !capitalize_array.empty?
+            first_str = capitalize_array.shift 
+            if first_str.include?("-") && capitalize_array.count == 0
+                capitalize_array.unshift(first_str)
+            elsif first_str.include?("-") && capitalize_array.count > 0
+                capitalize_array.unshift(first_str)
+            elsif first_str == first_str.downcase
+                capitalize_array.unshift(first_str.capitalize)
+            else
+                capitalize_array.unshift(first_str.capitalize)
+            end.join(" ")
+        end
     end
 end
