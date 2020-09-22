645596034e804c49d21d410136947d78388b7c62
Alvin Coulson <aaacoulson@gmail.com>
2020-09-21 23:57:49 -0400

    add uniqueness validation to project title attribute

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,7 @@ require_relative 'concerns/slugifiable.rb'
 class Project < ActiveRecord::Base
     belongs_to :user
     validates :title, presence: true
+    validates_uniqueness_of :title, case_sensitive: false
     validates :url, format: { with: /\:\/+\w+\.[a-zA-Z]{2,}/, message: "must be valid" },
         if: Proc.new { |entry| entry.url.present? }
 
