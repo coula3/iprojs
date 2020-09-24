104917e6b35f5a1a183de2ab895362ab450fcd48
Alvin Coulson <aaacoulson@gmail.com>
2020-09-23 22:53:15 -0400

    add format validation for project title

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,6 +4,7 @@ class Project < ActiveRecord::Base
     belongs_to :user
     validates :title, presence: true
     validates_uniqueness_of :title, case_sensitive: false
+    validates :title, format: { with: /\A[a-zA-Z0-9-:\s]+\z/, message: "only allows letters, numbers, spaces, colons and hyphens" }
     validates :url, format: { with: /\:\/+\w+\.[a-zA-Z]{2,}/, message: "must be valid" },
         if: Proc.new { |entry| entry.url.present? }
 
