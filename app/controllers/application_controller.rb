92ed73e33e156e35270085def4e41a04f6007672
Alvin Coulson <aaacoulson@gmail.com>
2020-09-24 21:35:14 -0400

    add logic to compute the percentage of on-time project completion for dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,8 +24,10 @@ class ApplicationController < Sinatra::Base
     if current_user && current_user.projects.empty?
       redirect :"/about"
     elsif logged_in?
-      @last_project_created = current_user.projects.sort_by {|p| p.created_at}.last
-      @last_project_updated = current_user.projects.sort_by {|p| p.updated_at}.last
+      @last_project_created = current_user.projects.sort_by {|p| p.created_at if p.created_at}.last
+      @last_project_updated = current_user.projects.sort_by {|p| p.updated_at if p.updated_at}.last
+      @rate_of_timely_completion = get_rate_of_timely_completion
+
       erb :"/dashboard.html"
     else
       redirect :"/signin"
@@ -39,6 +41,12 @@ class ApplicationController < Sinatra::Base
 
     def current_user
       @user ||= User.find_by(id: session[:id]) if session[:id]
+    end
+
+    def get_rate_of_timely_completion
+      completed_projects = User.first.projects.select {|p| p.phase == "Completed" || p.phase == "Production"}
+      on_time_completion = completed_projects.select {|p| p.planned_end_date >= p.actual_end_date}
+      ((on_time_completion.size / completed_projects.size.to_f) * 100).round(1)
     end
   end
 end
