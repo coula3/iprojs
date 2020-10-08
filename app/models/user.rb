977bb8e84cd5e5b5936572aab08f72ec62420dd3
Alvin Coulson <aaacoulson@gmail.com>
2020-10-08 01:41:20 -0400

    fire format validation on first and last names only if fields are not empty

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,13 @@ class User < ActiveRecord::Base
     has_many :projects
     
     validates :first_name, :last_name, :date_of_birth, :email, presence: true
-    validates :first_name, :last_name, format: { with: /\A[a-zA-Z-\s]+\z/, message: "only allows letters, space and hyphen" }
+    validates :first_name, :last_name, format: { with: /\A[a-zA-Z\-\s]+\z/, message: "only allows letters, space and hyphen" }, :if => lambda{first_name.present? && last_name.present?}
     validates :email, email: true
     validates_uniqueness_of :email, case_sensitive: false
     validate :dob_must_be_at_least_ten_years_old
     has_secure_password
 
-    validates :password, confirmation: true, :if => lambda{ new_record? || !password.nil? }
+    validates :password, confirmation: true, :if => lambda{ new_record? && password.present? }
 
     def dob_must_be_at_least_ten_years_old
         if date_of_birth.present? && calculate_age < 10
