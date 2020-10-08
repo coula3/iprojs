fea3c060790b0b603217005eb4688fe1e699ca08
Alvin Coulson <aaacoulson@gmail.com>
2020-10-07 23:53:40 -0400

    fix bug on project title validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ActiveRecord::Base
     belongs_to :user
     validates :title, :start_date, :planned_end_date, presence: true
     validates_uniqueness_of :title, case_sensitive: false
-    validates :title, format: { with: /\A[a-zA-Z0-9-:\s]+\z/, message: "only allows letters, numbers, spaces, colons and hyphens" }
+    validates :title, format: { with: /\A[a-zA-Z0-9-:'\s]+\z/, message: "only allows letters, numbers single quotes, colons and hyphens" }
     validates :url, format: { with: /\:\/+\w+\.[a-zA-Z]{2,}/, message: "must be valid" },
         if: Proc.new { |entry| entry.url.present? }
 
