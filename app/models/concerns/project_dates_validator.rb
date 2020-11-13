128fe53f4b22dc26f2744d55733ea2a9dba5293b
Alvin Coulson <aaacoulson@gmail.com>
2020-11-12 23:23:00 -0500

    fix bug on 'ProjectDatesValidator'

--- app/models/concerns/project_dates_validator.rb
+++ app/models/concerns/project_dates_validator.rb
@@ -1,11 +1,11 @@
 class ProjectDatesValidator < ActiveModel::Validator
     def validate(record)
         if record.actual_end_date && record.actual_end_date > Time.now
-            record.errors[:base] << "Actual End Date must only be today's date or earlier"
+            record.errors[:base] << "Actual End Date must not be a future date"
         elsif record.planned_end_date && (record.planned_end_date < record.start_date)
             record.errors[:base] << "Planned End Date must not be earlier than Start Date"
-        elsif record.actual_end_date && (record.actual_end_date < record.planned_end_date)
-            record.errors[:base] << "Actual End Date must not be earlier than Planned End Date"
+        elsif record.actual_end_date && (record.actual_end_date < record.start_date)
+            record.errors[:base] << "Actual End Date must not be earlier than Start Date"
         end
     end
 end
